09124439a6a897563fb44ddb1a29dfa44369ae5f
Haoming <hmstudy02@gmail.com>
2023-11-30 12:47:58 +0800

    optimization

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
 ﻿# ComfyUI Node Beautify
-A simple program to organize the workflow.
+A simple program to format the workflow
 
-### Features
-Round the sizes and the positions of each node in a workflow to a preset value.
+### Feature
+Round the `size` and the `position` of each node in a workflow by a preset value.
 
 ### How to Use
 1. Download the executable from **Release**
-2. Drag and drop the saved workflow `.json` files onto the `.exe`
+2. Drag and drop the saved workflow `.json` file(s) onto the `.exe`; or launch the `.exe` and enter a path
 3. ...
 4. ~~Profit~~
 
-### Build Yourself
-You can also clone the repo and build from source yourself. 
-Within the script, you can modify the `GRID_SIZE` constant to change the values to round to.
-You can also add entries to `FIXED_SIZE`, to set a constant size for all nodes of the specified type.
+### Configs
+You can clone the repo and edit the source code. 
+Within the script, you can modify the `GRID_SIZE` constant to change the values to round to;
+You can also add entries to the `FIXED_SIZE` dictionary, to set a constant size for nodes of specified type.
--- src/Program.cs
+++ src/Program.cs
@@ -5,77 +5,115 @@ namespace NodeBeautify
 {
     internal class Program
     {
-        private const int GRID_SIZE = 10;
+        /// <summary>
+        /// Round to the cloest multiples of this value
+        /// </summary>
+        private const int GRID_SIZE = 20;
 
+        /// <summary>
+        /// NodeType, (Width, Height)
+        /// </summary>
         private static readonly Dictionary<string, (float, float)> FIXED_SIZE = new(){
             {
-                "CLIPTextEncode", (400.0f, 150.0f)
+                "CLIPTextEncode", (420.0f, 160.0f)
             }
         };
 
+        private static bool hasError;
         static void Main(string[] args)
         {
+            hasError = false;
+
             if (args.Length == 0)
             {
-                Console.WriteLine("Drag & Drop a JSON File onto the Executable!");
+                Console.Write("Path to Workflow: ");
+                string input = Console.ReadLine()?.Trim();
+                ParseFilePath(input);
+            }
+            else
+            {
+                foreach (string filePath in args)
+                    ParseFilePath(filePath.Trim());
+            }
+
+            if (hasError)
                 Pause();
+        }
+
+        private static void ParseFilePath(string path)
+        {
+            if (path.StartsWith('\"') && path.EndsWith('\"'))
+                path = path.Trim('"');
+
+            if (!File.Exists(path) || !path.EndsWith(".json"))
+            {
+                Console.WriteLine($"Invalid File: {path}");
+                hasError = true;
                 return;
             }
 
-            foreach (string filePath in args)
-            {
-                if (!File.Exists(filePath) || !filePath.EndsWith(".json"))
-                {
-                    Console.WriteLine($"Invalid File: {filePath}");
-                    Pause();
-                    continue;
-                }
+            Process(path);
+        }
 
-                Process(filePath);
-            }
+        private static float Round(JToken value)
+        {
+            return (float)value - ((float)value % GRID_SIZE);
         }
 
         private static void Process(string path)
         {
-            string jsonContent = File.ReadAllText(path);
+            string jsonContent = File.ReadAllText(path).Trim();
 
             try
             {
                 var data = JsonConvert.DeserializeObject<JObject>(jsonContent);
-                JArray nodes = (JArray)data["nodes"];
+                var nodes = (JArray)data["nodes"];
 
+                // Doing try-catch cause somehow the value can be either array or dictionary...
                 foreach (JObject node in nodes)
                 {
-                    JArray pos = (JArray)node["pos"];
-                    pos[0] = GRID_SIZE * MathF.Round(pos[0].Value<float>() / GRID_SIZE);
-                    pos[1] = GRID_SIZE * MathF.Round(pos[1].Value<float>() / GRID_SIZE);
+                    try
+                    {
+                        JArray pos = (JArray)node["pos"];
+                        pos[0] = Round(pos[0]);
+                        pos[1] = Round(pos[1]);
+                    }
+                    catch (InvalidCastException)
+                    {
+                        JObject pos = (JObject)node["pos"];
+                        pos["0"] = Round(pos["0"]);
+                        pos["1"] = Round(pos["1"]);
+                    }
+
+                    string key = (string)node["type"];
 
                     try
                     {
                         JObject size = (JObject)node["size"];
-                        if (FIXED_SIZE.ContainsKey((string)node["type"]))
+
+                        if (FIXED_SIZE.ContainsKey(key))
                         {
-                            size["0"] = FIXED_SIZE[(string)node["type"]].Item1;
-                            size["1"] = FIXED_SIZE[(string)node["type"]].Item2;
+                            size["0"] = FIXED_SIZE[key].Item1;
+                            size["1"] = FIXED_SIZE[key].Item2;
                         }
                         else
                         {
-                            size["0"] = GRID_SIZE * MathF.Round(size["0"].Value<float>() / GRID_SIZE);
-                            size["1"] = GRID_SIZE * MathF.Round(size["1"].Value<float>() / GRID_SIZE);
+                            size["0"] = Round(size["0"]);
+                            size["1"] = Round(size["1"]);
                         }
                     }
                     catch (InvalidCastException)
                     {
                         JArray size = (JArray)node["size"];
-                        if (FIXED_SIZE.ContainsKey((string)node["type"]))
+                        if (FIXED_SIZE.ContainsKey(key))
                         {
-                            size[0] = FIXED_SIZE[(string)node["type"]].Item1;
-                            size[1] = FIXED_SIZE[(string)node["type"]].Item2;
+                            size[0] = FIXED_SIZE[key].Item1;
+                            size[1] = FIXED_SIZE[key].Item2;
                         }
                         else
                         {
-                            size[0] = GRID_SIZE * MathF.Round(size[0].Value<float>() / GRID_SIZE);
-                            size[1] = GRID_SIZE * MathF.Round(size[1].Value<float>() / GRID_SIZE);
+                            size[0] = Round(size[0]);
+                            size[1] = Round(size[0]);
                         }
                     }
                 }
@@ -85,12 +123,12 @@ namespace NodeBeautify
             catch (NullReferenceException)
             {
                 Console.WriteLine($"File {path} is not a valid workflow!");
-                Pause();
+                hasError = true;
             }
             catch (Exception e)
             {
-                Console.WriteLine($"Something went wrong while processing: {path}!\n\n{e}\n");
-                Pause();
+                Console.WriteLine($"Something went wrong while processing file {path}!\n\n{e}\n");
+                hasError = true;
             }
         }
 
