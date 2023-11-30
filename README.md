258d91819a7570ce51939d58a29c43842652a482
Haoming <hmstudy02@gmail.com>
2023-11-30 10:20:56 +0800

    init

---
+++ README.md
@@ -0,0 +1,16 @@
+﻿# ComfyUI Node Beautify
+A simple program to organize the workflow.
+
+### Features
+Round the sizes and the positions of each node in a workflow to a preset value.
+
+### How to Use
+1. Download the executable from **Release**
+2. Drag and drop the saved workflow `.json` files onto the `.exe`
+3. ...
+4. ~~Profit~~
+
+### Build Yourself
+You can also clone the repo and build from source yourself. 
+Within the script, you can modify the `GRID_SIZE` constant to change the values to round to.
+You can also add entries to `FIXED_SIZE`, to set a constant size for all nodes of the specified type.
---
+++ src/Program.cs
@@ -0,0 +1,103 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace NodeBeautify
+{
+    internal class Program
+    {
+        private const int GRID_SIZE = 10;
+
+        private static readonly Dictionary<string, (float, float)> FIXED_SIZE = new(){
+            {
+                "CLIPTextEncode", (400.0f, 150.0f)
+            }
+        };
+
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Drag & Drop a JSON File onto the Executable!");
+                Pause();
+                return;
+            }
+
+            foreach (string filePath in args)
+            {
+                if (!File.Exists(filePath) || !filePath.EndsWith(".json"))
+                {
+                    Console.WriteLine($"Invalid File: {filePath}");
+                    Pause();
+                    continue;
+                }
+
+                Process(filePath);
+            }
+        }
+
+        private static void Process(string path)
+        {
+            string jsonContent = File.ReadAllText(path);
+
+            try
+            {
+                var data = JsonConvert.DeserializeObject<JObject>(jsonContent);
+                JArray nodes = (JArray)data["nodes"];
+
+                foreach (JObject node in nodes)
+                {
+                    JArray pos = (JArray)node["pos"];
+                    pos[0] = GRID_SIZE * MathF.Round(pos[0].Value<float>() / GRID_SIZE);
+                    pos[1] = GRID_SIZE * MathF.Round(pos[1].Value<float>() / GRID_SIZE);
+
+                    try
+                    {
+                        JObject size = (JObject)node["size"];
+                        if (FIXED_SIZE.ContainsKey((string)node["type"]))
+                        {
+                            size["0"] = FIXED_SIZE[(string)node["type"]].Item1;
+                            size["1"] = FIXED_SIZE[(string)node["type"]].Item2;
+                        }
+                        else
+                        {
+                            size["0"] = GRID_SIZE * MathF.Round(size["0"].Value<float>() / GRID_SIZE);
+                            size["1"] = GRID_SIZE * MathF.Round(size["1"].Value<float>() / GRID_SIZE);
+                        }
+                    }
+                    catch (InvalidCastException)
+                    {
+                        JArray size = (JArray)node["size"];
+                        if (FIXED_SIZE.ContainsKey((string)node["type"]))
+                        {
+                            size[0] = FIXED_SIZE[(string)node["type"]].Item1;
+                            size[1] = FIXED_SIZE[(string)node["type"]].Item2;
+                        }
+                        else
+                        {
+                            size[0] = GRID_SIZE * MathF.Round(size[0].Value<float>() / GRID_SIZE);
+                            size[1] = GRID_SIZE * MathF.Round(size[1].Value<float>() / GRID_SIZE);
+                        }
+                    }
+                }
+
+                File.WriteAllText(path, JsonConvert.SerializeObject(data));
+            }
+            catch (NullReferenceException)
+            {
+                Console.WriteLine($"File {path} is not a valid workflow!");
+                Pause();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Something went wrong while processing: {path}!\n\n{e}\n");
+                Pause();
+            }
+        }
+
+        private static void Pause()
+        {
+            Console.WriteLine("Press ENTER to Continue...");
+            while (Console.ReadKey().Key != ConsoleKey.Enter) { }
+        }
+    }
+}
