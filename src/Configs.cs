1b4a553d94f2d558a1310b4af9efab8f0a8cc100
Haoming <hmstudy02@gmail.com>
2023-12-11 15:41:12 +0800

    sort nodes

---
+++ src/Configs.cs
@@ -0,0 +1,18 @@
+﻿namespace NodeBeautify
+{
+    internal static class Configs
+    {
+        /// <summary>
+        /// Round to the cloest multiples of this value
+        /// </summary>
+        public const int GRID_SIZE = 10;
+
+        /// <summary>
+        /// NodeType, (Width, Height)
+        /// </summary>
+        public static readonly Dictionary<string, (float, float)> FIXED_SIZE = new(){
+            { "CLIPTextEncode", (400.0f, 160.0f) },
+            { "PreviewImage", (420.0f, 420.0f) }
+        };
+    }
+}
